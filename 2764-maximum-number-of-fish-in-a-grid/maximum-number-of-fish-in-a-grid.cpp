813f764f8d96d88b1b9c4a41abb608939383feac
Vishesh Nagar <134713466+Vishesh-Nagar@users.noreply.github.com>
2025-01-28 12:06:03 +0530

    Time: 3 ms (84.06%) | Memory: 96.6 MB (56.04%) - LeetSync

--- 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.cpp
+++ 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.cpp
@@ -1,37 +1,25 @@
 class Solution {
 public:
     int helper(vector<vector<int>>& grid, vector<vector<bool>>& visited, int row, int col) {
-        int n = grid.size(), m = grid[0].size(), val = 0;
-        queue<pair<int, int>> q;
-        q.push({row, col});
-        visited[row][col] = true;
-        vector<int> dr = {0, 0, 1, -1};
-        vector<int> dc = {1, -1, 0, 0};
-        while (!q.empty()) {
-            row = q.front().first;
-            col = q.front().second;
-            q.pop();
-            val += grid[row][col];
-            for (int i = 0; i < 4; i++) {
-                int nr = row + dr[i];
-                int nc = col + dc[i];
-                if (0 <= nr && nr < n && 0 <= nc && nc < m && grid[nr][nc] &&
-                    !visited[nr][nc]) {
-                    q.push({nr, nc});
-                    visited[nr][nc] = true;
-                }
-            }
+        if (row < 0 || row >= grid.size() || col < 0 || col >= grid[0].size() ||
+            grid[row][col] == 0 || visited[row][col]) {
+            return 0;
         }
-        return val;
+        visited[row][col] = true;
+        return grid[row][col] + (helper(grid, visited, row, col + 1) +
+                                 helper(grid, visited, row, col - 1) +
+                                 helper(grid, visited, row + 1, col) +
+                                 helper(grid, visited, row - 1, col));
     }
 
     int findMaxFish(vector<vector<int>>& grid) {
-        int n = grid.size(), m = grid[0].size(), ans = 0;
-        vector<vector<bool>> visited(n, vector<bool>(m));
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] && !visited[i][j]) {
-                    ans = max(ans, helper(grid, visited, i, j));
+        int n = grid.size(), m = grid[0].size();
+        int ans = 0;
+        vector<vector<bool>> visited(n, vector<bool>(m, false));
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < m; col++) {
+                if (grid[row][col] > 0 && !visited[row][col]) {
+                    ans = max(ans, helper(grid, visited, row, col));
                 }
             }
         }
