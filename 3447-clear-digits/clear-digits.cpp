379fc2f74464b0f8de415b2b87210bc425ae1221
Vishesh Nagar <134713466+Vishesh-Nagar@users.noreply.github.com>
2025-02-10 11:31:19 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (70.42%) - LeetSync

--- 3447-clear-digits/clear-digits.cpp
+++ 3447-clear-digits/clear-digits.cpp
@@ -1,23 +1,14 @@
 class Solution {
 public:
-    bool isDigit(char c) {
-        int x = c - '0';
-        return x >= 0 && x <= 9;
-    }
     string clearDigits(string s) {
-        stack<char> st;
+        string ans;
         for (char c : s) {
-            if (isDigit(c) && !st.empty()) {
-                st.pop();
-            } else if (!isDigit(c))
-                st.push(c);
+            if (isdigit(c)) {
+                if (!ans.empty())
+                    ans.pop_back();
+            } else
+                ans.push_back(c);
         }
-        string ans = "";
-        while (!st.empty()) {
-            ans.push_back(st.top());
-            st.pop();
-        }
-        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
