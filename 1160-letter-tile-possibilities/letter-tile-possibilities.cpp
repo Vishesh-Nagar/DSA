745ec83cd7c6a6800324667a05cda4b26c096562
Vishesh Nagar <134713466+Vishesh-Nagar@users.noreply.github.com>
2025-02-17 11:32:46 +0530

    Time: 3 ms (78.62%) | Memory: 12.2 MB (51.72%) - LeetSync

--- 1160-letter-tile-possibilities/letter-tile-possibilities.cpp
+++ 1160-letter-tile-possibilities/letter-tile-possibilities.cpp
@@ -1,22 +1,40 @@
 class Solution {
 public:
     int numTilePossibilities(string tiles) {
-        vector<int> count(26, 0);
-        for (char c : tiles)
-            count[c - 'A']++;
-        return helper(count);
+        unordered_set<string> st;
+        sort(tiles.begin(), tiles.end());
+        return helper(tiles, "", 0, st) - 1;
     }
 
-    int helper(vector<int>& count) {
-        int ans = 0;
-        for (int i = 0; i < 26; i++) {
-            if (count[i] == 0)
-                continue;
-            ans++;
-            count[i]--;
-            ans += helper(count);
-            count[i]++;
+    int factorial(int n) {
+        if (n <= 1)
+            return 1;
+        int result = 1;
+        for (int num = 2; num <= n; num++)
+            result *= num;
+        return result;
+    }
+
+    int permutations(string& seq) {
+        vector<int> count(26, 0);
+        for (char ch : seq)
+            count[ch - 'A']++;
+        int total = factorial(seq.length());
+        for (int count : count) {
+            if (count > 1)
+                total /= factorial(count);
         }
-        return ans;
+        return total;
+    }
+
+    int helper(string& tiles, string current, int pos,
+               unordered_set<string>& st) {
+        if (pos >= tiles.length()) {
+            if (st.insert(current).second)
+                return permutations(current);
+            return 0;
+        }
+        return helper(tiles, current, pos + 1, st) +
+               helper(tiles, current + tiles[pos], pos + 1, st);
     }
 };
