2799e0714a9b7172bbbe28f5f8fb81af76229815
Vishesh Nagar <134713466+Vishesh-Nagar@users.noreply.github.com>
2025-02-23 12:25:16 +0530

    Time: 3 ms (39.83%) | Memory: 28 MB (90.13%) - LeetSync

--- 925-construct-binary-tree-from-preorder-and-postorder-traversal/construct-binary-tree-from-preorder-and-postorder-traversal.cpp
+++ 925-construct-binary-tree-from-preorder-and-postorder-traversal/construct-binary-tree-from-preorder-and-postorder-traversal.cpp
@@ -1,28 +1,21 @@
 class Solution {
 public:
     TreeNode* constructFromPrePost(vector<int>& pre, vector<int>& post) {
-        int n = pre.size();
-        vector<int> v(n + 1);
-        for (int index = 0; index < n; index++)
-            v[post[index]] = index;
-        return helper(0, n - 1, 0, pre, v);
+        int preIndex = 0;
+        int postIndex = 0;
+        return constructTree(preIndex, postIndex, pre, post);
     }
 
-    TreeNode* helper(int preStart, int preEnd, int postStart, vector<int>& pre,
-                     vector<int>& v) {
-        if (preStart > preEnd)
-            return NULL;
+    TreeNode* constructTree(int& preIndex, int& postIndex, vector<int>& pre, vector<int>& post) {
+        TreeNode* root = new TreeNode(pre[preIndex]);
+        preIndex++;
+        if (root->val != post[postIndex])
+            root->left = constructTree(preIndex, postIndex, pre, post);
 
-        if (preStart == preEnd)
-            return new TreeNode(pre[preStart]);
-
-        int leftRoot = pre[preStart + 1];
-        int nleft = v[leftRoot] - postStart + 1;
-
-        TreeNode* root = new TreeNode(pre[preStart]);
-        root->left = helper(preStart + 1, preStart + nleft, postStart, pre, v);
-        root->right =
-            helper(preStart + nleft + 1, preEnd, postStart + nleft, pre, v);
+        if (root->val != post[postIndex])
+            root->right = constructTree(preIndex, postIndex, pre, post);
+        
+        postIndex++;
         return root;
     }
 };
