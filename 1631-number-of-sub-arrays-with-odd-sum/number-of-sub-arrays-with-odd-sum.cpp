1a8ab8b2da0fc88d7c476260a28fc830f7acc663
Vishesh Nagar <134713466+Vishesh-Nagar@users.noreply.github.com>
2025-02-25 10:36:05 +0530

    Time: 0 ms (100.00%) | Memory: 112.1 MB (55.17%) - LeetSync

--- 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.cpp
+++ 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.cpp
@@ -1,31 +1,20 @@
 class Solution {
 public:
-    const int MOD = 1e9 + 7;
     int numOfSubarrays(vector<int>& arr) {
-        int n = arr.size();
-        for (auto& num : arr)
-            num %= 2;
-
-        vector<int> dpEven(n), dpOdd(n);
-        if (arr[n - 1] == 0)
-            dpEven[n - 1] = 1;
-        else
-            dpOdd[n - 1] = 1;
-
-        for (int num = n - 2; num >= 0; num--) {
-            if (arr[num] == 1) {
-                dpOdd[num] = (1 + dpEven[num + 1]) % MOD;
-                dpEven[num] = dpOdd[num + 1];
+        const int MOD = 1e9 + 7;
+        int ans = 0, prefixSum = 0;
+        int odd = 0, even = 1;
+        for (int num : arr) {
+            prefixSum += num;
+            if (prefixSum % 2 == 0) {
+                ans += odd;
+                even++;
             } else {
-                dpEven[num] = (1 + dpEven[num + 1]) % MOD;
-                dpOdd[num] = dpOdd[num + 1];
+                ans += even;
+                odd++;
             }
+            ans %= MOD;
         }
-        int count = 0;
-        for (auto oddCount : dpOdd) {
-            count += oddCount;
-            count %= MOD;
-        }
-        return count;
+        return ans;
     }
 };
