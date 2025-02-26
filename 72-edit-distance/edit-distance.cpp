cfa3a3ec4e400522ef12fd4f66995135d13394da
Vishesh Nagar <134713466+Vishesh-Nagar@users.noreply.github.com>
2025-02-26 16:55:14 +0530

    Time: 11 ms (30.89%) | Memory: 13.1 MB (42.24%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
-    int memo(string& word1, string& word2, int m, int n, vector<vector<int>>& dp) {
-        if (m == 0)
-            return dp[m][n] = n;
-        if (n == 0)
-            return dp[m][n] = m;
-
-        if (dp[m][n] != -1)
-            return dp[m][n];
-
-        if (word1[m - 1] == word2[n - 1])
-            return dp[m][n] = memo(word1, word2, m - 1, n - 1, dp);
-        else {
-            int insertChar = memo(word1, word2, m, n - 1, dp);
-            int deleteChar = memo(word1, word2, m - 1, n, dp);
-            int replaceChar = memo(word1, word2, m - 1, n - 1, dp);
-            return dp[m][n] = 1 + min({insertChar, deleteChar, replaceChar});
-        }
-    }
-
     int minDistance(string word1, string word2) {
         int m = word1.length(), n = word2.length();
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));
-        return memo(word1, word2, m, n, dp);
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        for (int i = 0; i < m + 1; i++)
+            dp[i][0] = i;
+
+        for (int j = 0; j < n + 1; j++)
+            dp[0][j] = j;
+
+        for (int i = 1; i < m + 1; i++) {
+            for (int j = 1; j < n + 1; j++) {
+                if (word1[i - 1] == word2[j - 1])
+                    dp[i][j] = dp[i - 1][j - 1];
+                else {
+                    int insertChar = dp[i][j - 1];
+                    int deleteChar = dp[i - 1][j];
+                    int replaceChar = dp[i - 1][j - 1];
+                    dp[i][j] = 1 + min({insertChar, deleteChar, replaceChar});
+                }
+            }
+        }
+        return dp[m][n];
     }
 };
