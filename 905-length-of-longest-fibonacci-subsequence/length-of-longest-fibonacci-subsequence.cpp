1f600451a6eb611ecc401519352476d2060fa327
Vishesh Nagar <134713466+Vishesh-Nagar@users.noreply.github.com>
2025-02-27 12:27:07 +0530

    Time: 408 ms (13.88%) | Memory: 150.1 MB (13.25%) - LeetSync

--- 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.cpp
+++ 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     int lenLongestFibSubseq(vector<int>& arr) {
-        int n = arr.size();
-        unordered_set<int> st(arr.begin(), arr.end());
         int ans = 0;
-        for (int start = 0; start < n; ++start) {
-            for (int next = start + 1; next < n; ++next) {
-                int prev = arr[next];
-                int curr = arr[start] + arr[next];
-                int len = 2;
-                while (st.find(curr) != st.end()) {
-                    int temp = curr;
-                    curr += prev;
-                    prev = temp;
-                    len++;
-                    ans = max(ans, len);
-                }
+        vector<vector<int>> dp(arr.size(), vector<int>(arr.size(), 0));
+        unordered_map<int, int> mp;
+        for (int i = 0; i < arr.size(); i++) {
+            mp[arr[i]] = i;
+            for (int j = 0; j < i; j++) {
+                int diff = arr[i] - arr[j];
+                int prev = (mp.find(diff) != mp.end()) ? mp[diff] : -1;
+                if (diff < arr[j] && prev >= 0)
+                    dp[j][i] = dp[prev][j] + 1;
+                else
+                    dp[j][i] = 2;
+                ans = max(ans, dp[j][i]);
             }
         }
-        return ans;
+        return ans > 2 ? ans : 0;
     }
 };
