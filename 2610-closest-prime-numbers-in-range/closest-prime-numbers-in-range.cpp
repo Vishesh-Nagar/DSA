c0fc685b9a4c8228c00f2e5d6b25855e52e09b09
Vishesh Nagar <134713466+Vishesh-Nagar@users.noreply.github.com>
2025-03-07 13:08:45 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (93.15%) - LeetSync

--- 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.cpp
+++ 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.cpp
@@ -1,41 +1,37 @@
 class Solution {
 public:
-    vector<int> sieve(int upperLimit) {
-        vector<int> sieve(upperLimit + 1, 1);
-        sieve[0] = 0;
-        sieve[1] = 0;
-        for (int n = 2; n * n <= upperLimit; n++) {
-            if (sieve[n] == 1) {
-                for (int multiple = n * n; multiple <= upperLimit; multiple += n)
-                    sieve[multiple] = 0;
-            }
+    bool isPrime(int n) {
+        if (n < 2)
+            return false;
+        if (n == 2 || n == 3)
+            return true;
+        if (n % 2 == 0)
+            return false;
+        for (int i = 3; i * i <= n; i += 2) {
+            if (n % i == 0)
+                return false;
         }
-        return sieve;
+        return true;
     }
 
     vector<int> closestPrimes(int left, int right) {
-        int upperLimit = right;
-        vector<int> sieveArray = sieve(upperLimit);
-        vector<int> primes;
-        for (int num = left; num <= right; num++) {
-            if (sieveArray[num] == 1)
-                primes.push_back(num);
-        }
-
-        if (primes.size() < 2)
-            return vector<int>{-1, -1};
-
-        int minDifference = INT_MAX;
-        vector<int> closestPair(2, -1);
-
-        for (int index = 1; index < primes.size(); index++) {
-            int difference = primes[index] - primes[index - 1];
-            if (difference < minDifference) {
-                minDifference = difference;
-                closestPair[0] = primes[index - 1];
-                closestPair[1] = primes[index];
+        int prevPrime = -1, cA = -1, cB = -1;
+        int diff = 1e6;
+        for (int i = left; i <= right; i++) {
+            if (isPrime(i)) {
+                if (prevPrime != -1) {
+                    int difference = i - prevPrime;
+                    if (difference < diff) {
+                        diff = difference;
+                        cA = prevPrime;
+                        cB = i;
+                    }
+                    if (difference == 2 or difference == 1)
+                        return {prevPrime, i};
+                }
+                prevPrime = i;
             }
         }
-        return closestPair;
+        return (cA == -1) ? vector<int>{-1, -1} : vector<int>{cA, cB};
     }
 };
