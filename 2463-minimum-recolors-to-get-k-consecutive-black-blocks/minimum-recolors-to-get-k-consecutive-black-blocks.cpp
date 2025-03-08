aaade2433aebcc803a6c8169198793c017a15bf0
Vishesh Nagar <134713466+Vishesh-Nagar@users.noreply.github.com>
2025-03-08 11:07:10 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (62.68%) - LeetSync

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.cpp
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.cpp
@@ -1,26 +1,16 @@
 class Solution {
 public:
     int minimumRecolors(string blocks, int k) {
-        queue<char> q;
-        int count = 0;
-        while (q.size() < k) {
-            char c = blocks[q.size()];
-            if (c == 'W')
+        int l = 0, count = 0, ans = INT_MAX;
+        for (int r = 0; r < blocks.size(); r++) {
+            if (blocks[r] == 'W')
                 count++;
-            q.push(c);
-        }
-
-        int ans = count;
-        for (int i = k; i < blocks.size(); i++) {
-            if (q.front() == 'W')
-                count--;
-            q.pop();
-
-            if (blocks[i] == 'W')
-                count++;
-            q.push(blocks[i]);
-
-            ans = min(ans, count);
+            if (r - l + 1 == k) {
+                ans = min(ans, count);
+                if (blocks[l] == 'W')
+                    count--;
+                l++;
+            }
         }
         return ans;
     }
