e1906c3256d6dcec5f577ea3828e37d118722720
Vishesh Nagar <134713466+Vishesh-Nagar@users.noreply.github.com>
2025-03-10 16:38:50 +0530

    Time: 168 ms (45.50%) | Memory: 16 MB (61.23%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
     int countPalindromicSubsequence(string s) {
-        unordered_set<char> st;
-        for (char c : s)
-            st.insert(c);
+        int n = s.length();
+        vector<pair<int, int>> indices(26, {-1, -1});
+
+        for (int i = 0; i < n; i++) {
+            char ch = s[i];
+            int idx = ch - 'a';
+
+            if (indices[idx].first == -1)
+                indices[idx].first = i;
+
+            indices[idx].second = i;
+        }
+
         int ans = 0;
-        for (char c : st) {
-            int i = -1;
-            int j = 0;
-            for (int k = 0; k < s.size(); k++) {
-                if (s[k] == c) {
-                    if (i == -1)
-                        i = k;
-                    j = k;
-                }
-            }
-            unordered_set<char> between;
-            for (int k = i + 1; k < j; k++) {
-                between.insert(s[k]);
-            }
-            ans += between.size();
+        for (int i = 0; i < 26; i++) {
+            if (indices[i] == make_pair(-1, -1))
+                continue;
+
+            unordered_set<char> st;
+            for (int m = indices[i].first + 1; m <= indices[i].second - 1; m++)
+                st.insert(s[m]);
+
+            ans += st.size();
         }
         return ans;
     }
