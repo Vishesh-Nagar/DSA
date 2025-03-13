ad3160ba196329148fde45fd83c75f11e76913bd
Vishesh Nagar <134713466+Vishesh-Nagar@users.noreply.github.com>
2025-03-13 10:47:17 +0530

    Time: 5 ms (87.50%) | Memory: 322.9 MB (85.62%) - LeetSync

--- 3643-zero-array-transformation-ii/zero-array-transformation-ii.cpp
+++ 3643-zero-array-transformation-ii/zero-array-transformation-ii.cpp
@@ -1,35 +1,25 @@
 class Solution {
 public:
     int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
-        int n = nums.size(), left = 0, right = queries.size();
-        if (!helper(nums, queries, right))
-            return -1;
-
-        while (left <= right) {
-            int middle = left + (right - left) / 2;
-            if (helper(nums, queries, middle))
-                right = middle - 1;
-            else
-                left = middle + 1;
-        }
-        return left;
-    }
-
-    bool helper(vector<int>& nums, vector<vector<int>>& queries, int k) {
-        int n = nums.size(), sum = 0;
+        int n = nums.size(), sum = 0, k = 0;
         vector<int> diff(n + 1);
 
-        for (int idx = 0; idx < k; idx++) {
-            int start = queries[idx][0], end = queries[idx][1], val = queries[idx][2];
-            diff[start] += val;
-            diff[end + 1] -= val;
-        }
+        for (int i = 0; i < n; i++) {
+            while (sum + diff[i] < nums[i]) {
+                k++;
+                if (k > queries.size())
+                    return -1;
 
-        for (int numIndex = 0; numIndex < n; numIndex++) {
-            sum += diff[numIndex];
-            if (sum < nums[numIndex])
-                return false;
+                int left = queries[k - 1][0], right = queries[k - 1][1],
+                    val = queries[k - 1][2];
+
+                if (right >= i) {
+                    diff[max(left, i)] += val;
+                    diff[right + 1] -= val;
+                }
+            }
+            sum += diff[i];
         }
-        return true;
+        return k;
     }
 };
