c59402c9814700d28eb06949096845f974d5cc26
Vishesh Nagar <134713466+Vishesh-Nagar@users.noreply.github.com>
2025-03-26 12:33:00 +0530

    Time: 47 ms (42.86%) | Memory: 94.5 MB (15.80%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
@@ -2,21 +2,34 @@ class Solution {
 public:
     int minOperations(vector<vector<int>>& grid, int x) {
         vector<int> nums;
-        int ans = 0;
+        int ans = INT_MAX;
+
         for (int row = 0; row < grid.size(); row++) {
             for (int col = 0; col < grid[0].size(); col++) {
+                if (grid[row][col] % x != grid[0][0] % x)
+                    return -1;
                 nums.push_back(grid[row][col]);
             }
         }
 
-        int n = nums.size();
-        nth_element(nums.begin(), nums.begin() + n / 2, nums.end());
-        int num = nums[n / 2];
+        sort(nums.begin(), nums.end());
 
-        for (int number : nums) {
-            if (number % x != num % x)
-                return -1;
-            ans += abs(num - number) / x;
+        int n = nums.size();
+        vector<int> pref(n, 0);
+        vector<int> suff(n, 0);
+
+        for (int i = 1; i < n; i++)
+            pref[i] = pref[i - 1] + nums[i - 1];
+
+        for (int i = n - 2; i >= 0; i--)
+            suff[i] = suff[i + 1] + nums[i + 1];
+
+        for (int i = 0; i < n; i++) {
+            int left = (nums[i] * i - pref[i]) / x;
+
+            int right = (suff[i] - nums[i] * (n - i - 1)) / x;
+
+            ans = min(ans, left + right);
         }
 
         return ans;
