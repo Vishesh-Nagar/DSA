92b6b9259c104b0d9c1f488f1582fddd259c4b4f
Vishesh Nagar <134713466+Vishesh-Nagar@users.noreply.github.com>
2025-03-29 01:21:29 +0530

    Time: 94 ms (68.48%) | Memory: 51.8 MB (35.87%) - LeetSync

--- 2588-maximum-number-of-points-from-grid-queries/maximum-number-of-points-from-grid-queries.cpp
+++ 2588-maximum-number-of-points-from-grid-queries/maximum-number-of-points-from-grid-queries.cpp
@@ -1,59 +1,107 @@
+class Cell {
+public:
+    int row;
+    int col;
+    int value;
+    Cell(int r, int c, int v) : row(r), col(c), value(v) {}
+};
+
+class Query {
+public:
+    int index;
+    int value;
+    Query(int i, int v) : index(i), value(v) {}
+};
+
+bool operator<(const Query& a, const Query& b) {
+    return a.value < b.value;
+}
+
+bool operator<(const Cell& a, const Cell& b) {
+    return a.value < b.value;
+}
+
+class UnionFind {
+private:
+    vector<int> parent;
+    vector<int> size;
+
+public:
+    UnionFind(int n) : parent(vector<int>(n, -1)), size(vector<int>(n, 1)) {}
+
+    int find(int node) {
+        if (parent[node] < 0)
+            return node;
+
+        return parent[node] = find(parent[node]);
+    }
+
+    bool union_nodes(int nodeA, int nodeB) {
+        int rootA = find(nodeA);
+        int rootB = find(nodeB);
+        if (rootA == rootB)
+            return false;
+
+        if (size[rootA] > size[rootB]) {
+            parent[rootB] = rootA;
+            size[rootA] += size[rootB];
+        } else {
+            parent[rootA] = rootB;
+            size[rootB] += size[rootA];
+        }
+        return true;
+    }
+
+    int getSize(int node) {
+        return size[find(node)];
+    }
+};
+
+const vector<int> drow = {0, 0, 1, -1};
+const vector<int> dcol = {1, -1, 0, 0};
+
 class Solution {
 public:
-#define ai3 array<int, 3>
-    static constexpr int dir[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-
     vector<int> maxPoints(vector<vector<int>>& grid, vector<int>& queries) {
-        int queryCount = queries.size();
-        vector<int> result(queryCount);
-        int n = grid.size();
-        int m = grid[0].size();
+        int n = grid.size(), m = grid[0].size();
+        int totalCells = n * m;
 
-        int cells = n * m;
-        vector<int> threshold(cells + 1);
+        vector<Query> sortedQueries;
+        for (int i = 0; i < queries.size(); i++)
+            sortedQueries.push_back(Query(i, queries[i]));
 
-        vector<vector<int>> val(n, vector<int>(m, INT_MAX));
+        sort(sortedQueries.begin(), sortedQueries.end());
 
-        val[0][0] = grid[0][0];
+        vector<Cell> sortedCells;
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < m; col++)
+                sortedCells.push_back(Cell(row, col, grid[row][col]));
+        }
 
-        priority_queue<ai3, vector<ai3>, greater<ai3>> pq;
-        pq.push({grid[0][0], 0, 0});
-        int visited = 0;
+        sort(sortedCells.begin(), sortedCells.end());
 
-        while (!pq.empty()) {
-            ai3 curr = pq.top();
-            pq.pop();
+        UnionFind uf = UnionFind(totalCells);
+        vector<int> result(queries.size());
+        int idx = 0;
 
-            threshold[++visited] = curr[0];
+        for (Query query : sortedQueries) {
+            while (idx < totalCells && sortedCells[idx].value < query.value) {
+                int row = sortedCells[idx].row;
+                int col = sortedCells[idx].col;
+                int cellId = row * m + col;
 
-            for (const auto& d : dir) {
-                int nrow = curr[1] + d[0];
-                int ncol = curr[2] + d[1];
+                for (int i = 0; i < 4; i++) {
+                    int nrow = row + drow[i];
+                    int ncol = col + dcol[i];
 
-                if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m &&
-                    val[nrow][ncol] == INT_MAX) {
-                    val[nrow][ncol] = max(curr[0], grid[nrow][ncol]);
-
-                    pq.push({val[nrow][ncol], nrow, ncol});
+                    if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && grid[nrow][ncol] < query.value)
+                        uf.union_nodes(cellId, nrow * m + ncol);
+                
                 }
+                idx++;
             }
+            result[query.index] = query.value > grid[0][0] ? uf.getSize(0) : 0;
         }
-
-        for (int i = 0; i < queryCount; i++) {
-            int limit = queries[i];
-            int left = 0, right = cells;
-
-            while (left < right) {
-                int mid = left + (right - left + 1) / 2;
-                if (threshold[mid] < limit)
-                    left = mid;
-                else
-                    right = mid - 1;
-            }
-
-            result[i] = left;
-        }
-
         return result;
     }
 };
