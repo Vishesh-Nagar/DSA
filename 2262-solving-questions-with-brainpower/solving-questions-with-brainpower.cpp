5f97c92439816ae2e4f44701083bf64330539eb2
Vishesh Nagar <134713466+Vishesh-Nagar@users.noreply.github.com>
2025-04-01 15:16:19 +0530

    Time: 8 ms (49.73%) | Memory: 119.5 MB (95.63%) - LeetSync

--- 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
-    long long rec(vector<vector<int>>& q, int index, vector<long long>& dp) {
-        if (index >= q.size())
-            return 0;
-        if (dp[index] != -1)
-            return dp[index];
-        long long take = q[index][0] + rec(q, index + q[index][1] + 1, dp);
-        long long notTake = rec(q, index + 1, dp);
-        return dp[index] = max(take, notTake);
-    }
-
     long long mostPoints(vector<vector<int>>& q) {
-        vector<long long> dp(q.size(), -1);
-        long long ans = rec(q, 0, dp);
-        return ans;
+        int n = q.size();
+        vector<long long> dp(n + 1, 0);
+        for (int i = n - 1; i >= 0; i--) {
+            long long take = q[i][0] + (i + q[i][1] + 1 < n ? dp[i + q[i][1] + 1]
+                                                            : 0);
+            long long notTake = dp[i + 1];
+            dp[i] = max(take, notTake);
+        }
+        return dp[0];
     }
 };
