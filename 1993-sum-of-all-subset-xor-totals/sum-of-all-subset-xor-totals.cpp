fee01b6e25ba1b427a0601fa4e8bc1dcb208b453
Vishesh Nagar <134713466+Vishesh-Nagar@users.noreply.github.com>
2025-04-05 12:28:06 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (90.00%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
@@ -1,29 +1,14 @@
 class Solution {
 public:
-    void gen(const vector<int>& nums, int index, vector<int> curr, vector<vector<int>>& subsets) {
-        if (index == nums.size()) {
-            subsets.push_back(curr);
-            return;
-        }
-
-        curr.push_back(nums[index]);
-        gen(nums, index + 1, curr, subsets);
-
-        curr.pop_back();
-        gen(nums, index + 1, curr, subsets);
+    int XORSum(vector<int>& nums, int idx, int curr) {
+        if (idx == nums.size())
+            return curr;
+        int take = XORSum(nums, idx + 1, curr ^ nums[idx]);
+        int notTake = XORSum(nums, idx + 1, curr);
+        return take + notTake;
     }
 
     int subsetXORSum(vector<int>& nums) {
-        vector<vector<int>> subsets;
-        gen(nums, 0, {}, subsets);
-
-        int ans = 0;
-        for (auto& it : subsets) {
-            int subsetXORTotal = 0;
-            for (int num : it)
-                subsetXORTotal ^= num;
-            ans += subsetXORTotal;
-        }
-        return ans;
+        return XORSum(nums, 0, 0);
     }
 };
