2cb34ab83ba49360961bea0a27f19d86da224b13
Vishesh Nagar <134713466+Vishesh-Nagar@users.noreply.github.com>
2025-04-14 01:13:43 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (69.95%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.cpp
+++ 2050-count-good-numbers/count-good-numbers.cpp
@@ -4,9 +4,8 @@ public:
     long long modExp(long long base, long long exp, long long mod) {
         long long res = 1;
         while (exp > 0) {
-            if (exp % 2 == 1) {
+            if (exp % 2 == 1)
                 res = (res * base) % mod;
-            }
             base = (base * base) % mod;
             exp /= 2;
         }
