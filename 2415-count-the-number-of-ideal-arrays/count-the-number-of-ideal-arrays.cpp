2c04e7e3cdb4f309fb87416e4adf1a1f380331a3
Vishesh Nagar <134713466+Vishesh-Nagar@users.noreply.github.com>
2025-04-22 12:53:54 +0530

    Time: 74 ms (39.45%) | Memory: 29.7 MB (34.86%) - LeetSync

--- 2415-count-the-number-of-ideal-arrays/count-the-number-of-ideal-arrays.cpp
+++ 2415-count-the-number-of-ideal-arrays/count-the-number-of-ideal-arrays.cpp
@@ -1,47 +1,28 @@
-const int MOD = 1e9 + 7, MAX_N = 1e4 + 10, MAX_P = 15;
-int c[MAX_N + MAX_P][MAX_P + 1];
-vector<int> ps[MAX_N];
-int sieve[MAX_N];
-
 class Solution {
 public:
-    Solution() {
-        if (c[0][0])
-            return;
-        for (int i = 2; i < MAX_N; i++) {
-            if (sieve[i] == 0) {
-                for (int j = i; j < MAX_N; j += i)
-                    sieve[j] = i;
-            }
-        }
-        for (int i = 2; i < MAX_N; i++) {
-            int x = i;
-            while (x > 1) {
-                int p = sieve[x];
-                int cnt = 0;
-                while (x % p == 0) {
-                    x /= p;
-                    cnt++;
-                }
-                ps[i].push_back(cnt);
-            }
-        }
-        c[0][0] = 1;
-        for (int i = 1; i < MAX_N + MAX_P; i++) {
-            c[i][0] = 1;
-            for (int j = 1; j <= min(i, MAX_P); j++)
-                c[i][j] = (c[i - 1][j] + c[i - 1][j - 1]) % MOD;
-        }
-    }
-
+    int mod = 1e9 + 7;
     int idealArrays(int n, int maxValue) {
-        long long ans = 0;
-        for (int x = 1; x <= maxValue; x++) {
-            long long mul = 1;
-            for (int p : ps[x])
-                mul = mul * c[n + p - 1][p] % MOD;
-            ans = (ans + mul) % MOD;
+        vector<vector<int>> dp(15, vector<int>(maxValue + 1, 0));
+        for (int i = 1; i <= maxValue; i++)
+            dp[1][i] = 1;
+        for (int len = 2; len < 15; len++) {
+            for (int i = 1; i <= maxValue; i++) {
+                for (int j = 2 * i; j <= maxValue; j += i)
+                    dp[len][j] = (dp[len][j] + dp[len - 1][i]) % mod;
+            }
         }
-        return ans;
+        vector<vector<int>> comb(n + 1, vector<int>(15, 0));
+        for (int i = 0; i <= n; i++) {
+            comb[i][0] = 1;
+            for (int j = 1; j < 15 && j <= i; j++)
+                comb[i][j] = (comb[i - 1][j] + comb[i - 1][j - 1]) % mod;
+        }
+        int res = 0;
+        for (int len = 1; len < 15; len++) {
+            for (int x = 1; x <= maxValue; x++) {
+                res = (res + 1LL * dp[len][x] * comb[n - 1][len - 1] % mod) % mod;
+            }
+        }
+        return res;
     }
 };
