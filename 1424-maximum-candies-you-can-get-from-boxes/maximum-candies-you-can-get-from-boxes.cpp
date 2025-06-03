ebf8b81392a37ac07bc1d458724e3dfded48b68b
Vishesh Nagar <134713466+Vishesh-Nagar@users.noreply.github.com>
2025-06-03 16:38:55 +0530

    Time: 16 ms (20.67%) | Memory: 44 MB (78.85%) - LeetSync

--- 1424-maximum-candies-you-can-get-from-boxes/maximum-candies-you-can-get-from-boxes.cpp
+++ 1424-maximum-candies-you-can-get-from-boxes/maximum-candies-you-can-get-from-boxes.cpp
@@ -1,5 +1,29 @@
 class Solution {
 public:
+    int dfs(int box, vector<int>& status, vector<int>& candies,
+            vector<vector<int>>& keys, vector<vector<int>>& containedBoxes,
+            vector<bool>& vis, vector<bool>& boxPresent) {
+        if (!status[box] || vis[box])
+            return 0;
+        vis[box] = true;
+        int res = candies[box];
+        for (int k : keys[box]) {
+            status[k] = 1;
+            if (boxPresent[k] && !vis[k]) {
+                res += dfs(k, status, candies, keys, containedBoxes, vis,
+                           boxPresent);
+            }
+        }
+        for (int b : containedBoxes[box]) {
+            boxPresent[b] = true;
+            if (status[b] && !vis[b]) {
+                res += dfs(b, status, candies, keys, containedBoxes, vis,
+                           boxPresent);
+            }
+        }
+        return res;
+    }
+
     int maxCandies(vector<int>& status, vector<int>& candies,
                    vector<vector<int>>& keys,
                    vector<vector<int>>& containedBoxes,
@@ -7,33 +31,23 @@ public:
         int n = status.size();
         vector<bool> vis(n, false);
         vector<bool> boxPresent(n, false);
-        queue<int> q;
-        for (int b : initialBoxes) {
+        int total = 0;
+
+        for (int b : initialBoxes)
             boxPresent[b] = true;
-            if (status[b])
-                q.push(b);
-        }
 
-        int ans = 0;
-        while (!q.empty()) {
-            int box = q.front();
-            q.pop();
-            if (vis[box])
-                continue;
-            vis[box] = true;
-            ans += candies[box];
+        bool found;
+        do {
+            found = false;
+            for (int i = 0; i < n; i++) {
+                if (boxPresent[i] && status[i] && !vis[i]) {
+                    total += dfs(i, status, candies, keys, containedBoxes, vis,
+                                 boxPresent);
+                    found = true;
+                }
+            }
+        } while (found);
 
-            for (int k : keys[box]) {
-                status[k] = 1;
-                if (boxPresent[k] && !vis[k])
-                    q.push(k);
-            }
-            for (int b : containedBoxes[box]) {
-                boxPresent[b] = true;
-                if (status[b] && !vis[b])
-                    q.push(b);
-            }
-        }
-        return ans;
+        return total;
     }
 };
