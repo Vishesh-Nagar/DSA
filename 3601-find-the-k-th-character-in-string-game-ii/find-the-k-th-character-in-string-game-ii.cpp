24dfe8bba37fc57a8ac68683d29ddf9e01353c14
Vishesh Nagar <134713466+Vishesh-Nagar@users.noreply.github.com>
2025-07-04 13:04:34 +0530

    Time: 0 ms (100.00%) | Memory: 28.3 MB (94.09%) - LeetSync

--- 3601-find-the-k-th-character-in-string-game-ii/find-the-k-th-character-in-string-game-ii.cpp
+++ 3601-find-the-k-th-character-in-string-game-ii/find-the-k-th-character-in-string-game-ii.cpp
@@ -2,14 +2,10 @@ class Solution {
 public:
     char kthCharacter(long long k, vector<int>& operations) {
         int ans = 0;
-        int t;
-        while (k != 1) {
-            t = __lg(k);
-            if (((long long)1 << t) == k)
-                t--;
-            k = k - ((long long)1 << t);
-            if (operations[t])
-                ans++;
+        k--;
+        for (int i = __lg(k); i >= 0; i--) {
+            if (k >> i & 1)
+                ans += operations[i];
         }
         return 'a' + (ans % 26);
     }
