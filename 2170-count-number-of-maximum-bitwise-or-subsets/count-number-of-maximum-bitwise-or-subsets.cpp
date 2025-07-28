f90c4b6bca127c96a07d69ce0e58f6d1a648d1a1
Vishesh Nagar <134713466+Vishesh-Nagar@users.noreply.github.com>
2025-07-28 11:05:44 +0530

    Time: 103 ms (20.29%) | Memory: 310.1 MB (10.75%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
     int countMaxOrSubsets(vector<int>& nums) {
+        int n = nums.size();
         int maxOR = 0;
         for (int num : nums)
             maxOR |= num;
-        return count(nums, 0, 0, maxOR);
+
+        vector<vector<int>> dp(n, vector<int>(maxOR + 1, -1));
+        return count(nums, 0, 0, maxOR, dp);
     }
 
-    int count(vector<int>& nums, int index, int currOR, int targetOR) {
+    int count(vector<int>& nums, int index, int currOR, int targetOr,
+              vector<vector<int>>& dp) {
         if (index == nums.size())
-            return (currOR == targetOR) ? 1 : 0;
+            return (currOR == targetOr) ? 1 : 0;
 
-        int take = count(nums, index + 1, currOR | nums[index], targetOR);
-        int notTake = count(nums, index + 1, currOR, targetOR);
-        return notTake + take;
+        if (dp[index][currOR] != -1)
+            return dp[index][currOR];
+
+        int take = count(nums, index + 1, currOR | nums[index], targetOr, dp);
+        int notTake = count(nums, index + 1, currOR, targetOr, dp);
+
+        return dp[index][currOR] = notTake + take;
     }
 };
