8252c4441432cc467fa7be0eba807c69e8dfacae
Vishesh Nagar <134713466+Vishesh-Nagar@users.noreply.github.com>
2025-08-20 10:26:48 +0530

    Time: 3 ms (82.96%) | Memory: 27.7 MB (88.20%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
-    int solve(int i, int j, vector<vector<int>>& grid,
-              vector<vector<int>>& dp) {
-        if (i >= grid.size() || j >= grid[0].size())
-            return 0;
-        if (grid[i][j] == 0)
-            return 0;
+    int countSquares(vector<vector<int>>& matrix) {
+        int row = matrix.size(), col = matrix[0].size(), ans = 0, prev = 0;
+        vector<int> dp(col + 1, 0);
 
-        if (dp[i][j] != -1)
-            return dp[i][j];
-
-        int right = solve(i, j + 1, grid, dp);
-        int diagonal = solve(i + 1, j + 1, grid, dp);
-        int below = solve(i + 1, j, grid, dp);
-
-        return dp[i][j] = 1 + min(right, min(diagonal, below));
-    }
-
-    int countSquares(vector<vector<int>>& grid) {
-        int ans = 0;
-        vector<vector<int>> dp(grid.size(), vector<int>(grid[0].size(), -1));
-        for (int i = 0; i < grid.size(); i++) {
-            for (int j = 0; j < grid[0].size(); j++) {
-                ans += solve(i, j, grid, dp);
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= col; j++) {
+                if (matrix[i - 1][j - 1] == 1) {
+                    int temp = dp[j];
+                    dp[j] = 1 + min(prev, min(dp[j - 1], dp[j]));
+                    prev = temp;
+                    ans += dp[j];
+                } else {
+                    dp[j] = 0;
+                }
             }
         }
         return ans;
