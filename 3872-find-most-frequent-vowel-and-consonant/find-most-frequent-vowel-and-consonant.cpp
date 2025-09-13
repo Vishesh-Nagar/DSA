07b25aee30c12551b340c5d652cab1292829f8bf
Vishesh Nagar <134713466+Vishesh-Nagar@users.noreply.github.com>
2025-09-13 12:12:32 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (81.58%) - LeetSync

--- 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.cpp
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
-    bool isVowel(char c) {
-        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
-    }
-
     int maxFreqSum(string s) {
         vector<int> freq(26, 0);
-        for (char c : s)
-            freq[c - 'a']++;
         int a = 0, b = 0;
-        for (int i = 0; i < 26; i++) {
-            if (freq[i] > 0) {
-                if (isVowel('a' + i))
-                    a = max(a, freq[i]);
-                else
-                    b = max(b, freq[i]);
-            }
+        for (char c : s) {
+            int i = c - 'a';
+            freq[i]++;
+            if (i == 0 || i == 4 || i == 8 || i == 14 || i == 20)
+                a = max(a, freq[i]);
+            else
+                b = max(b, freq[i]);
         }
         return a + b;
     }
