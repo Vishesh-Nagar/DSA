f18e4a75962723a20ba94c9f26878cffc4b3e5a0
Vishesh Nagar <134713466+Vishesh-Nagar@users.noreply.github.com>
2025-10-13 07:20:44 +0000

    Time: 3 ms (48.84%) | Memory: 16.1 MB (75.29%) - LeetSync

--- 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.cpp
+++ 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
-    bool compare(const string& w1, const string& w2) {
-        vector<int> f(26);
-        for (char c : w1)
-            ++f[c - 'a'];
-        for (char c : w2)
-            --f[c - 'a'];
-        return all_of(f.begin(), f.end(), [](int x) {
-            return x == 0;
-        });
-    }
-
     vector<string> removeAnagrams(vector<string>& words) {
-        int n = words.size();
-        vector<string> res = {words[0]};
-        for (int i = 1; i < n; ++i) {
-            if (compare(words[i], words[i - 1]))
-                continue;
-            res.push_back(words[i]);
+        vector<string> ans;
+        ans.push_back(words[0]);
+
+        for (int i = 1; i < words.size(); i++) {
+            string a = words[i], b = ans.back();
+            sort(a.begin(), a.end()), sort(b.begin(), b.end());
+            if (a != b)
+                ans.push_back(words[i]);
         }
-        return res;
+        return ans;
     }
 };
