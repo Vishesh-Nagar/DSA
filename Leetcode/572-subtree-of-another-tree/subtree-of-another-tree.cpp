cbf4429880b776b6b5a873b39eb63b2c2c66b259
Vishesh Nagar <134713466+Vishesh-Nagar@users.noreply.github.com>
2025-10-29 16:53:25 +0530

    Time: 0 ms (100.00%) | Memory: 29 MB (72.17%) - LeetSync

---
+++ Leetcode/572-subtree-of-another-tree/subtree-of-another-tree.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isSame(TreeNode* s, TreeNode* t) {
+        if (s == NULL || t == NULL)
+            return s == NULL && t == NULL;
+        else if (s->val == t->val)
+            return isSame(s->left, t->left) && isSame(s->right, t->right);
+        else
+            return false;
+    }
+    bool isSubtree(TreeNode* s, TreeNode* t) {
+        if (s == NULL)
+            return false;
+        else if (isSame(s, t))
+            return true;
+        else
+            return isSubtree(s->left, t) || isSubtree(s->right, t);
+    }
+};
