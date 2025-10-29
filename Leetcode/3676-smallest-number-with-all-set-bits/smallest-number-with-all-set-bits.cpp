154802f03fe59756396f174873118d43fa9b68db
Vishesh Nagar <134713466+Vishesh-Nagar@users.noreply.github.com>
2025-10-29 13:13:09 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (11.95%) - LeetSync

--- Leetcode/3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.cpp
+++ Leetcode/3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.cpp
@@ -1,21 +1,7 @@
 class Solution {
 public:
     int smallestNumber(int n) {
-        string s = "";
-        while (n) {
-            if (n % 2 == 0)
-                s.push_back('0');
-            else
-                s.push_back('1');
-            n /= 2;
-        }
-        for (int i = 0; i < s.size(); i++)
-            s[i] = '1';
-        int ans = 0, p = 1;
-        for (char c : s) {
-            ans += p;
-            p *= 2;
-        }
-        return ans;
+        int k = ceil(log2(n + 1));
+        return pow(2, k) - 1;
     }
 };
