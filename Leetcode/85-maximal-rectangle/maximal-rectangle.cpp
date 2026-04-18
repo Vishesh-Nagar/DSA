8f24923b4fa0c6850c0cb17d0eaf84a6b86434f4
Vishesh Nagar <134713466+Vishesh-Nagar@users.noreply.github.com>
2026-04-18 13:12:39 +0530

    Time: 16 ms (22.68%) | Memory: 23.9 MB (9.84%) - LeetSync

--- Leetcode/85-maximal-rectangle/maximal-rectangle.cpp
+++ Leetcode/85-maximal-rectangle/maximal-rectangle.cpp
@@ -1,32 +1,56 @@
 class Solution {
 public:
-    int maximalRectangle(vector<vector<char>>& matrix) {
-        if (matrix.empty() || matrix[0].empty())
-            return 0;
-
-        int rows = matrix.size();
-        int cols = matrix[0].size();
-        vector<int> heights(cols + 1, 0);
-        int ans = 0;
-
-        for (const auto& row : matrix) {
-            for (int i = 0; i < cols; i++) {
-                heights[i] = (row[i] == '1') ? heights[i] + 1 : 0;
-            }
-
-            // Calculate max area using stack-based method
-            stack<int> st;
-            for (int i = 0; i < heights.size(); i++) {
-                while (!st.empty() && heights[i] < heights[st.top()]) {
-                    int h = heights[st.top()];
-                    st.pop();
-                    int w = st.empty() ? i : i - st.top() - 1;
-                    ans = max(ans, h * w);
-                }
-                st.push(i);
-            }
+    vector<int> NSL(vector<int>& arr) {
+        int n = arr.size();
+        stack<pair<int, int>> st;
+        vector<int> res(n);
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && st.top().first >= arr[i])
+                st.pop();
+            res[i] = st.empty() ? -1 : st.top().second;
+            st.push({arr[i], i});
         }
+        return res;
+    }
 
+    vector<int> NSR(vector<int>& arr) {
+        int n = arr.size();
+        stack<pair<int, int>> st;
+        vector<int> res(n);
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && st.top().first >= arr[i])
+                st.pop();
+            res[i] = st.empty() ? n : st.top().second;
+            st.push({arr[i], i});
+        }
+        return res;
+    }
+
+    int MAH(vector<int>& heights) {
+        int n = heights.size();
+        vector<int> left = NSL(heights);
+        vector<int> right = NSR(heights);
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            int area = (right[i] - left[i] - 1) * heights[i];
+            ans = max(ans, area);
+        }
+        return ans;
+    }
+
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        int n = matrix.size(), m = matrix[0].size();
+        int ans = 0;
+        vector<int> arr(m, 0);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == '1')
+                    arr[j]++;
+                else
+                    arr[j] = 0;
+            }
+            ans = max(ans, MAH(arr));
+        }
         return ans;
     }
 };
